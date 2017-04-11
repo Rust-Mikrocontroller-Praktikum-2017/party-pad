480b2d6acd786f9f4bdff267cd71386210debe10
TobiasGraef <tobias.graef28@gmail.com>
2017-04-11 19:49:18 +0200

    spectrum_visualizer2 uses DFT

--- src/visuals/spectrum_visualizer2.rs
+++ src/visuals/spectrum_visualizer2.rs
@@ -2,9 +2,13 @@ use visuals::Visualizer;
 use collections::boxed::Box;
 use super::STM;
 use visuals::constants::*;
+use transformation::DFT;
+use transformation::hamming;
 use audio;
 use core;
+use collections::Vec;
 
+const LENGTH: usize = 512;
 
 pub struct SpectrumVisualizer2<'a> {
     max: &'a mut [u16; X_MAX as usize],
@@ -12,40 +16,51 @@ pub struct SpectrumVisualizer2<'a> {
     bar_width: u16,
     color_bar: u16,
     color_max: u16,
+    dft: DFT,
+    signal: Vec<f32>,
+    spectrum: Vec<f32>,
 }
 impl<'a> Visualizer for SpectrumVisualizer2<'a> {
     fn draw(&mut self, mut stm: &mut STM) {
         let mode = false;
         //TODO cannot set length using bar_width
         //TODO use spectrum, not sound wave
-        let mut mic_input: [i16; (X_MAX / 2) as usize] = [0; (X_MAX / 2) as usize];
+        let mut mic_input: [i16; LENGTH] = [0; LENGTH];
         audio::get_microphone_input(&mut stm, &mut mic_input, mode);
 
+        //TODO put the following somewhere in mic input to save iterating?
+        //self.signal = mic_input.iter().enumerate().map(|(i, &v)| hamming::HAMMING_512[i] * v as f32 / core::i16::MAX as f32).collect();
+        
+        for i in 0..LENGTH {
+            self.signal[i] = hamming::HAMMING_512[i] * mic_input[i] as f32 / core::i16::MAX as f32;
+        }
+        self.dft.process(&self.signal, &mut self.spectrum);
+        
+        
         stm.lcd.clear_screen();
-        for i in 0..mic_input.len() {
+        for (x, &value) in self.spectrum.iter().take(120).enumerate() {
+            let x = (2 * x) as u16;
             //TODO scaling?
-            let scale_factor = mic_input[i] as f32 * 10.0 / core::i16::MAX as f32;
-            let value = (Y_MAX as f32 * scale_factor) as i32;
-            let value = if value < 0 { -value } else { value };
-            let value = core::cmp::min(value as u16, Y_MAX);
-            stm.draw_rectangle_filled(i as u16 * self.bar_width,
-                                      (i + 1) as u16 * self.bar_width,
+            let value = (100.0 * value) as u16;
+            let value = if value < 272 { value } else { 272 };
+            stm.draw_rectangle_filled(x as u16 * self.bar_width,
+                                      (x + 1) as u16 * self.bar_width,
                                       Y_MAX - value as u16,
                                       Y_MAX,
                                       self.color_bar);
             //save history
-            self.history[i] = value;
+            self.history[x as usize] = value;
             //handle max: new max or decrease
-            if value as u16 > self.max[i] {
-                self.max[i] = value;
+            if value as u16 > self.max[x as usize] {
+                self.max[x as usize] = value;
             } else {
-                self.max[i] = if self.max[i] > 0 {self.max[i] - 1} else {0};
+                self.max[x as usize] = if self.max[x as usize] > 0 { self.max[x as usize] - 1 } else { 0 };
             }
             //print max
-            stm.draw_rectangle_filled(i as u16 * self.bar_width,
-                                      (i + 1) as u16 * self.bar_width,
-                                      Y_MAX - self.max[i],
-                                      core::cmp::min(Y_MAX - self.max[i] + 2, Y_MAX),
+            stm.draw_rectangle_filled(x as u16 * self.bar_width,
+                                      (x + 1) as u16 * self.bar_width,
+                                      Y_MAX - self.max[x as usize],
+                                      core::cmp::min(Y_MAX - self.max[x as usize] + 2, Y_MAX),
                                       self.color_max);
         }
 
@@ -56,7 +71,7 @@ impl<'a> SpectrumVisualizer2<'a> {
                max: &'a mut [u16; X_MAX as usize],
                bar_width: u16,
                color_bar: u16,
-               color_max: u16,)
+               color_max: u16)
                -> Box<SpectrumVisualizer2<'a>> {
         Box::new(SpectrumVisualizer2 {
                      history: history,
@@ -64,6 +79,9 @@ impl<'a> SpectrumVisualizer2<'a> {
                      bar_width: bar_width,
                      color_bar: color_bar,
                      color_max: color_max,
+                     dft: DFT::new(LENGTH),
+                     signal: vec![0.0;LENGTH],
+                     spectrum: vec![0.0;LENGTH],
                  })
     }
 }
